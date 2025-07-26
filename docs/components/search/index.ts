456afaa2354591531a9083292af5f2820f018354
DoomsdayProd <doomsdayproductionsl33t@gmail.com>
2025-07-26 18:59:46 -0400

    Add files via upload

---
+++ docs/components/search/index.ts
@@ -0,0 +1,7 @@
+```typescript
+// Export all search components
+export { default as SearchBar } from './SearchBar.vue'
+export { default as SearchFilters } from './SearchFilters.vue'
+export { default as SearchInterface } from './SearchInterface.vue'
+export { default as SearchResults } from './SearchResults.vue'
+```
